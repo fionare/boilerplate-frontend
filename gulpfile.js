2fe75b00acd827c17cc97a0e8a6a5616557a1752
Brian S <brian@custom-media.com>
2021-01-07 18:58:54 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,4 @@ const runner = require("./gulp/default.js");
 exports.default = runner.default;
 exports.build = runner.build;
 exports.install = require("./gulp/install.js").default;
+exports.run = runner.run;
